3e7c7c593c3e61ba0e1ce8c2a4135c54a0481eab
Martin Meltsas <martin.meltsas@khk.ee>
2015-11-29 14:23:11 +0200

    basic login auth : confing.php removed [Delivers #108911886]

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DATABASE_HOSTNAME', 'localhost');
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME', 'pseudo_bank');
-define('WEBSITE_EMAIL', '');
--- config.sample.php
+++ config.sample.php
@@ -1,5 +1,5 @@
 <?php
-
+// change to config.php
 define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
